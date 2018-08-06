b6679e3ce9c5348a2b0274487242ded5bba8ec7b
imuck14@gmail.com <imuck14@gmail.com>
2018-08-06 18:14:53 +0200

    routing and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { CarteComponent } from './carte/carte.component';
+import { ReserveComponent } from './reserve/reserve.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes, Router } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent }, //http://localhost:4200
+  { path: 'carte', component: CarteComponent },//http://localhost:4200/carte
+  { path: 'reserve', component: ReserveComponent },//http://localhost:4200/reserve
+  { path: 'contact', component: ContactComponent } //http://localhost:4200/contact
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent,
+    CarteComponent,
+    ReserveComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
